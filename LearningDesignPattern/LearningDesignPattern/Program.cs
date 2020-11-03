0ac258ed1a3f8bcec69cda719c3f2a66e03b1e50
izumo <>
2020-11-03 14:13:15 +0900

    Create VS studio solution

---
+++ LearningDesignPattern/LearningDesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningDesignPattern
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
